55db9ec1e2632bd3974359b89c97005a836e5605
Armin <armin.babaei@me.com>
2025-01-16 19:10:53 -0500

    DEV-18
    https://linear.app/travelese/issue/DEV-18/travel

---
+++ apps/dashboard/src/app/[locale]/(app)/(sidebar)/itineraries/page.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Itineraries | Travelese",
+};
+
+export default async function Page() {
+
+  return (
+    <div className="mt-4">
+      Itineraries
+    </div>
+  );
+}
---
+++ apps/dashboard/src/app/[locale]/(app)/(sidebar)/stays/page.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Stays | Travelese",
+};
+
+export default async function Page() {
+
+  return (
+    <div className="mt-4">
+      Stays
+    </div>
+  );
+}
